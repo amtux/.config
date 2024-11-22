506acafc316d35d2e371ec148b13808be1a7159b
Amandeep Uppal <auppal@fastly.com>
2024-11-22 17:47:39 -0500

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,8 +3,8 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       -- swap fR with fr
-      { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
-      { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      -- { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+      -- { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
     },
   },
 }
