a038960d00d98bce73cd5b19e9c826a3153418b7
Amandeep Uppal <auppal@fastly.com>
2024-11-28 12:05:06 -0500

    update

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    --- @type TSConfig
+    opts = {
+      ensure_installed = {
+        "cue",
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
